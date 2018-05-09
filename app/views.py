1d87e99653b8fe84e1286de73e1fda5ae0d87edc
YoonJ <oyoons95@gmail.com>
2018-05-09 16:20:51 +0900

    message 3

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.csrf import csrf_exempt
 
 def keyboard(request):
     return JsonResponse(
@@ -9,14 +9,14 @@ def keyboard(request):
         }
     )
 
-@crsf_protect
+@crsf_exempt
 def message(request):
     return JsonResponse(
         {
             'message':{
                 'text': '메세지 동작'
             },
-            'keyboard':{c
+            'keyboard':{
                 'type':'text'
             }
         }
